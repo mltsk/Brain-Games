56f2befa98f0073b204e3c4c23b16fcf334167d4
mltsk <mltsk94@gmail.com>
2021-04-26 16:16:09 +0300

    step 9

--- Makefile
+++ Makefile
@@ -10,11 +10,11 @@ brain-gcd:
 	node bin/brain-gcd.js
 brain-progression:
 	node bin/brain-progression.js
+brain-prime:
+	node bin/brain-prime.js
 publish:
 	npm publish --dry-run
 lint:
 	npx eslint .
-lint fix:
-	npx eslint . --fix
 rec:
 	asciinema rec
---
+++ bin/brain-prime.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+import { getRandomInt } from '../src/cli.js';
+
+console.log('Welcome to the Brain Games!');
+const userName = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${userName}!`);
+console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+
+const isPrime = (num) => {
+    if ((num === 1) || (num === 2)) {
+        return 'yes';
+    }
+    for(let i = 2; i < num; i += 1) {
+        if(num % i === 0) {
+            return 'no';
+        }
+    }
+
+    return 'yes';
+}
+
+const gamePrime = () => {
+  for (let i = 0; i < 3; i += 1) {
+    const number1 = getRandomInt(400);
+
+    const correctAnswer = isPrime(number1);
+    console.log(`Question: ${number1}`);
+    const answer = readlineSync.question('You answer: ');
+
+    if (correctAnswer == answer) {
+      console.log('Correct!');
+      if (i === 2) {
+        console.log(`Congratulations, ${userName}`);
+      }
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'. Let's try again, ${userName}`);
+      break;
+    }
+  }
+};
+
+gamePrime();
