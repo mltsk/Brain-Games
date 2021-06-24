40d5be377e844ab56b56f598638686af9e8011d4
Dmitrii Maletskii <mltsk94@gmail.com>
2021-06-24 15:42:28 +0300

    refactoring function isPrime

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -6,7 +6,7 @@ import greeting from '../src/greeting.js';
 const userName = greeting('Answer "yes" if given number is prime. Otherwise answer "no".');
 
 const isPrime = (num) => {
-  if ((num === 1) || (num === 2)) return true;
+  if (num <= 2) return true;
   for (let i = 2; i <= num / 2; i += 1) {
     if (num % i === 0) return false;
   }
